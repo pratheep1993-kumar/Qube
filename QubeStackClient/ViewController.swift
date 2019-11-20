dec827b29a3880ca602be188d22545b61f3fc98f
Pratheep Kumar <pratheep@hipbar.com>
2019-11-20 10:18:25 +0530

    initial commit

---
+++ QubeStackClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QubeStackClient
+//
+//  Created by pratheepkumar on 20/11/19.
+//  Copyright © 2019 Qube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
